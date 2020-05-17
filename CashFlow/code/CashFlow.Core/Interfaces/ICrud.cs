5c2e16d1b632ab8010aaee2e9072c12f95646476
Carlos Henrique Gonçalves <carlos@minora.com.br>
2020-05-17 16:53:22 -0300

    Remoção do CashFlow

--- CashFlow/code/CashFlow.Core/Interfaces/ICrud.cs
+++
@@ -1,9 +0,0 @@
-namespace CashFlow.Core.Interfaces
-{
-    public interface ICrud<T>
-    {
-        T Get(long ID);
-        void Add(T model);
-        void Remove(T model);
-    }
-}
--- CashFlow/code/CashFlow.Core/Interfaces/IUser.cs
+++
@@ -1,9 +0,0 @@
-using CashFlow.Core.Models;
-
-namespace CashFlow.Core.Interfaces
-{
-    public interface IUser
-    {
-        void CheckInfo(User user);
-    }
-}
